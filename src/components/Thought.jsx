b0897cfe166fc4c237a6ce9b751cbabbc6faa9ca
Alexandra Franzen <alexandra.s.franzen@gmail.com>
2024-04-07 14:37:52 +0200

    Added css to components to create structure

--- src/components/Thought.jsx
+++ src/components/Thought.jsx
@@ -1,17 +1,49 @@
 import "./thought.css";
+import { useState } from "react";
 
-export const Thought = ({ message, hearts, time, loadingThoughts, getThought }) => {
+export const Thought = ({
+  id,
+  message,
+  hearts,
+  time,
+  loadingThoughts,
+  getThought,
+}) => {
+  const [likes, setLikes] = useState(hearts);
 
+  const handleClick = () => {
+    fetch(
+      `https://happy-thoughts-ux7hkzgmwa-uc.a.run.app/thoughts/${id}/like`,
+      {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+      }
+    )
+      .then((res) => res.json())
+      .then(() => {
+        setLikes(likes + 1);
+      })
+      .catch((error) => {
+        console.error(
+          "Something went wrong. Please try send some love again",
+          error
+        );
+      });
+  };
 
   return (
-    <div className="body">
+    <div className="thought-container">
       <div>{loadingThoughts ? "Loading thoughts..." : getThought}</div>
-      <div id="message">{message}</div>
+      <div className="message">{message}</div>
       <div>
-        <button>❤️x{hearts}</button>
+        <button
+          onClick={handleClick}
+          className={likes === 0 ? "heart-button" : "liked-button"}>
+          ❤️
+        </button>{" "}
+        x{hearts}
       </div>
       <div id="time">{time}</div>
     </div>
   );
 };
-
